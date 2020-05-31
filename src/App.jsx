efcbff689ba82de49efc298ad9e91057a71f4da4
Diego Rocha <diego.rocha@neon.com.br>
2020-05-31 15:47:01 -0300

    converting app to functional component

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import React, { Suspense } from 'react';
+import { Route, Switch } from 'react-router-dom';
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
+import Layout from './containers/Layout/Layout';
+
+const Checkout = React.lazy(() => {
+  return import('./containers/Checkout/Checkout');
+});
+
+const Orders = React.lazy(() => {
+  return import('./containers/Orders');
+});
+
+const app = (props) => {
+  const routes = (
+    <Switch>
+      <Route path="/checkout" render={() => <Checkout />} />
+      <Route path="/orders" render={() => <Orders />} />
+      <Route path="/" component={BurgerBuilder} />
+    </Switch>
+  );
+
+  return (
+    <div>
+      <Layout>
+        <Suspense fallback={<p>Loading...</p>}>{routes}</Suspense>
+      </Layout>
+    </div>
+  );
+};
+
+export default app;
